9c65adad2ef8bd00d9cec66e4c28e146326e0a05
Kevin Papst <kpapst@gmx.net>
2022-11-26 01:02:27 +0100

    fix null deprecation

--- Util/PasswordUpdater.php
+++ Util/PasswordUpdater.php
@@ -33,7 +33,7 @@ class PasswordUpdater implements PasswordUpdaterInterface
     {
         $plainPassword = $user->getPlainPassword();
 
-        if ($plainPassword || 0 === strlen($plainPassword)) {
+        if ($plainPassword === null || 0 === strlen($plainPassword)) {
             return;
         }
 
